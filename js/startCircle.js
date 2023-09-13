339b0bfa6fb36888290dbc6ff89215b7f46693e8
migerankin <244283586@qq.com>
2023-09-13 21:05:36 +0800

    first

---
+++ js/startCircle.js
@@ -0,0 +1,15 @@
+const m_arr = ['m_down','m_left','m_up','m_right']
+const f_arr = ['f_down','f_left','f_up','f_right']
+var m_num = 0, f_num = 0
+
+// startCircle()
+function startCircle(){
+    m_num=0
+    f_num=0
+    const startCircleCode = setInterval(() => {
+        $('.people').eq(1).find('img').attr('src',`./img/m/${m_arr[m_num++]}.png`)
+        if(m_num>3)m_num=0
+        $('.people').eq(0).find('img').attr('src',`./img/m/${f_arr[f_num++]}.png`)
+        if(f_num>3)f_num=0
+    }, 500);
+}
---
+++ js/walk.js
@@ -0,0 +1,49 @@
+var m_top = '200px'
+var m_left = '500px'
+var m_top_num = 200
+var m_left_num = 500
+var isclose = false
+// console.log(document.getElementsByClassName('boy')[0].style.left);
+
+document.addEventListener("keydown", function (event) {
+    m_top_num = parseInt(m_top.replace('px'))
+    m_left_num = parseInt(m_left.replace('px'))
+    if(parseInt(m_top.replace('px'))<0)m_top = '0px'
+    if(parseInt(m_left.replace('px'))<0)m_left = '0px'
+    if((m_top_num>200&&m_top_num<400)&&(m_left_num>700&&m_left_num<1100)){
+        document.getElementById('startF').style.display = 'block'
+        isclose=true
+    }
+    else document.getElementById('startF').style.display = 'none'
+    switch (event.key) {
+        case "w":
+            m_top=parseInt(m_top.replace('px'))-15+'px'
+            document.getElementsByClassName('people')[1].style
+            .top = m_top
+            $('.people').eq(1).find('img').attr('src',`./img/m/m_up.png`)
+            break;
+        case "a":
+            m_left=parseInt(m_left.replace('px'))-15+'px'
+            document.getElementsByClassName('people')[1].style
+            .left = m_left
+            $('.people').eq(1).find('img').attr('src',`./img/m/m_left.png`)
+            break;
+        case "d":
+            m_left=parseInt(m_left.replace('px'))+15+'px'
+            document.getElementsByClassName('people')[1].style
+            .left = m_left
+            $('.people').eq(1).find('img').attr('src',`./img/m/m_right.png`)
+            break;
+        case "s":
+            m_top=parseInt(m_top.replace('px'))+15+'px'
+            document.getElementsByClassName('people')[1].style
+            .top = m_top
+            $('.people').eq(1).find('img').attr('src',`./img/m/m_down.png`)
+            break;
+        case "f":
+            if(isclose)
+            document.getElementById('zj').play()
+            startCircle()
+            document.getElementById('startF').style.display = 'none'
+    }
+});
